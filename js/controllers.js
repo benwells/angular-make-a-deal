2afe9e1268e6c84d596e5c2f0ed94b1696c95bf9
Ben Wells <benwellsmusic@gmail.com>
2013-10-06 10:22:35 -0400

    conditional hover class

--- js/controllers.js
+++ js/controllers.js
@@ -1,16 +1,31 @@
 function DealController ($scope) {
 
-  var door = {
+  $scope.Door = function() {
+    this.path = "img/";
+    this.ext = ".png";
+  }
   
-  };
+  
 
-  var doors = [];
+  $scope.door1 = new $scope.Door();
+  $scope.door1.ind = 1;
+  $scope.door2 = new $scope.Door();
+  $scope.door2.ind = 2;
+  $scope.door3 = new $scope.Door();
+  $scope.door3.ind = 3;
+
+  $scope.doors = [];
+  $scope.doors.push($scope.door1);
+  $scope.doors.push($scope.door2);
+  $scope.doors.push($scope.door3);
+
+  $scope.hovered;
 
   $scope.init = function() {
 
   }
 
-  $scope.pulse = function(e) {
-    
+  $scope.hov = function(item) {
+    $scope.hovered = item;
   }
 }
